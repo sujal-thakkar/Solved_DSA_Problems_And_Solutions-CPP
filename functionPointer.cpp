b8807a4a9f0adeae03586cafbea9f239565126d0
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-01-06 19:58:16 +0530

    add new problem

---
+++ functionPointer.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a,int b) {
+    return a+b;
+}
+
+void greet(string name) {
+    cout << "Hello, " << name;
+}
+
+int main() {
+    int (*ptr)(int,int);
+
+    // ptr = &add;
+    // int c = (*ptr)(5, 4);
+    //OR,
+    ptr = add;
+    int c = ptr(5,4); // both are fairly right!
+    cout << c << '\n';
+
+    void (*p)(string);
+    p = greet;
+    p("jew");
+ 
+    return 0;
+}
