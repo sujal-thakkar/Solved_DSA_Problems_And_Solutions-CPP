7110cbe3e76e3fffa36932c4d9c4cb22c03aebb5
Sujal Thakkar <sujalthakkar153@gmail.com>
2025-04-19 08:23:00 +0530

    fix: correct calculation of left element size and simplify return logic in median function

--- NeetCode 150 sheet/Binary Search/median-of-two-sorted-arrays-optimal.cpp
+++ NeetCode 150 sheet/Binary Search/median-of-two-sorted-arrays-optimal.cpp
@@ -15,7 +15,7 @@
             int n1 = nums1.size(), n2 = nums2.size();// 1 5, 7 10 | 9 12, 18 15
             int n = n1 + n2;
             if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
-            int leftEleSize = (n1 + n2 + 1) / 2;
+            int leftEleSize = (n + 1) / 2;
             int l=0, r=n1;
             while(l <= r) {
                 int mid1 = l + (r-l)/2;
@@ -26,16 +26,11 @@
                 if(mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
                 if(n1 > mid1) r1 = nums1[mid1];
                 if(n2 > mid2) r2 = nums2[mid2];
-                if(l1 <= r2 and l2 <= r1) {
-                    if(n%2) return max(l1, l2);
-                    else return (double)(max(l1, l2) + min(r1, r2)) / 2.0;
-                }
-                else {
-                    if(l1 > r2) r = mid1 - 1;
-                    else l = mid1 + 1;
-                }
+                if(l1 > r2) r = mid1 - 1;
+                else if(l2 > r1) l = mid1 + 1;
+                else return (n % 2)? (double)max(l1, l2) : (double)(max(l1, l2) + min(r1, r2)) / 2.0;
             }
-            return 0;
+            return 0.0;
         }
     };
     int main() {
